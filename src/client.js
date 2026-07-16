870a87641af633fdd738ba4b8e00f5f89d6825fb
Sanjay Bharathi <sanjay.bharathi7@gmail.com>
2026-07-15 20:49:58 -0500

    Finished lab

---
+++ src/client.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+const URL = ''
+const API_KEY = ''
+
+export const supabase = createClient(URL, API_KEY)
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,21 @@ import { useState } from 'react'
 import './Card.css'
 import more from './more.png'
 import { Link } from 'react-router-dom'
-
+import { supabase } from '../client'
 
 const Card = (props) =>  {
 
-  const [count, setCount] = useState(0)
-  const updateCount = () => {
+  const [count, setCount] = useState(props.betCount)
+  const updateCount = async (event) => {
+    event.preventDefault()
+
+    await supabase
+      .from("Posts")
+      .update({
+        betCount: count + 1
+      })
+      .eq("id", props.id)
+  
     setCount((count) => count + 1)
   }
 
